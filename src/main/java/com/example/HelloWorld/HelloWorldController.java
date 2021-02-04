8f4847e396d48a626b7ddd06e8cad17afb43bc8d
nzhao <nyz18@my.fsu.edu>
2021-02-03 20:55:08 -0500

    Back to Original Hello World

--- src/main/java/com/example/HelloWorld/HelloWorldController.java
+++ src/main/java/com/example/HelloWorld/HelloWorldController.java
@@ -10,6 +10,6 @@ public class HelloWorldController {
     @RequestMapping("/")
     public String index()
     {
-        return "Hello World from intellij";
+        return "Hello World!.";
     }
 }
